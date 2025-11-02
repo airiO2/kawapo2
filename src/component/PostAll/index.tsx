56f21bd7883ca3a1a05d2e7581a8b72ba8f9f0af
airiO2 <otsu.airi6702@mail.kyutech.jp>
2025-11-02 20:25:36 +0900

    edit/post

--- src/component/PostAll/index.tsx
+++ src/component/PostAll/index.tsx
@@ -19,7 +19,7 @@ const Post = ({ icon, image, text1, text2, name }: PostProps) => {
   return (
     <div className={styles.PostAll}>
       <PostHeader icon={icon} name={name}></PostHeader>
-      {/* <Postimage link={image} tate={300} yoko={300}></Postimage> */}
+      <Postimage link={image} tate={300} yoko={300}></Postimage>
       <PostText text={text1}></PostText>
       <PostHashtag text={text2}></PostHashtag>
       
