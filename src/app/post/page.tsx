13fefa28cfca082a839da8f10789030664b59597
airiO2 <otsu.airi6702@mail.kyutech.jp>
2025-11-05 13:11:47 +0900

    edit/postpage

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 
 export default function post() {
   return (
-    <form action={posting}>
+    <form action={posting} className={styles.postform}>
       <Question></Question>
       <p>本文入力</p>
       <textarea className={styles.maintext} name="text"></textarea>
