c0f01c11776b6dd2c7e0044be63032c769ccbd1a
airiO2 <otsu.airi6702@mail.kyutech.jp>
2025-11-07 23:47:21 +0900

    edit/test

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,14 +1,19 @@
 import MyPageClient from '@/component/MyPageClient'
 import { createClient } from '@/utils/supabase/server'
+import Postimage from '@/component/PostImage'
+
 
 export default async function mypage() {
     const supabase = await createClient()
 
     const {
         data: { user },
+
+        
+
     } = await supabase.auth.getUser()
 
     return <MyPageClient user={user} />
-    
+
 }
 
--- src/component/MyPageClient/index.tsx
+++ src/component/MyPageClient/index.tsx
@@ -5,6 +5,7 @@ import { type User } from "@supabase/supabase-js";
 import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
 import styles from "./styles.module.css";
+import Postimage from "../PostImage";
 
 export default function MyPpage({ user }: { user: User | null }) {
   const supabase = createClient();
@@ -12,6 +13,9 @@ export default function MyPpage({ user }: { user: User | null }) {
   const [fullname, setFullname] = useState<string | null>(null);
   const [username, setUsername] = useState<string | null>(null);
   const [avatar_url, setAvatarUrl] = useState<string | null>(null);
+  const [image_url, setImageUrl] = useState<{img: string}[]>([]);
+
+// setAvatarUrlでavatar_urlを更新しているので、Avatarコンポーネントに正しいURLが渡される。
 
   const getProfile = useCallback(async () => {
     try {
@@ -40,9 +44,46 @@ export default function MyPpage({ user }: { user: User | null }) {
     }
   }, [user, supabase]);
 
-  useEffect(() => {
+  
+
+  const getPostImage = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const { data, error, status } = await supabase
+        .from("posts")
+        .select(`img`)
+        .eq("user_id", user?.id);
+        // "user_id"は、投稿テーブルの中のuser_idというカラム名を指している。
+      
+
+      console.log("Post Image Data:", data);
+
+      if (error && status !== 406) {
+         console.log(error);
+        throw error;
+      }
+      
+      if (data) {
+        setImageUrl(data);
+      }
+
+    } catch (error) {
+      alert("Error loading post image!");
+    } finally {
+      setLoading(false);
+    }
+  }, [user, supabase]);
+
+  useEffect(() =>{
+    
+    getPostImage();
     getProfile();
-  }, [user, getProfile]);
+  console.log("URL", image_url); 
+  },[user]);
+
+
+
 
   return (
     <>
@@ -58,6 +99,9 @@ export default function MyPpage({ user }: { user: User | null }) {
 
       <p className={styles.level}>かわいいレベル</p>
       <div className={styles.preview}></div>
+      {image_url.map((imageObj, index) => (
+        <Postimage key={index} link={imageObj.img} tate={100} yoko={100} />
+      ))}
     </>
   );
 }
